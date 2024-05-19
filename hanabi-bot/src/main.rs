6245f896d2d013ec3a34708222c0b7442305ae6a
Nikhil Jha <oss@nikhiljha.com>
2024-05-18 19:46:44 -0700

    :sparkles: add DM support to hanabi-bot

--- hanabi-bot/src/main.rs
+++ hanabi-bot/src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+mod test;
+
+use futures_util::stream::StreamExt;
+use futures_util::{SinkExt, TryStreamExt};
+use hanablive::messages::Message;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber)?;
+
+    let mut client = hanablive::client::Client::new();
+
+    let username = std::env::var("HANABI_USERNAME")?;
+    let password = std::env::var("HANABI_PASSWORD")?;
+
+    client.login(&username, &password).await?;
+    client.connect_ws().await?;
+
+    while let Ok(message) = client.try_next().await {
+        match message {
+            Some(Message::Chat(
+                   hanablive::messages::ChatData {
+                       msg,
+                       who: Some(who),
+                       room: Some(room),
+                       recipient: Some(recipient),
+                       ..
+                   })) =>
+                {
+                    if !recipient.eq(&username) {
+                        continue;
+                    }
+
+                    println!("PM from {}: {}", &who, &msg);
+                    client.send(Message::ChatPM(hanablive::messages::ChatPMData {
+                        msg: "Hello! I am a new work-in-progress H-Group bot. I don't work yet, but when I do you can DM me again and I'll explain how I work.".to_string(),
+                        recipient: who,
+                        room,
+                    })).await?;
+                }
+            Some(ref msg) => eprintln!("Unhandled message: {:?}", message),
+            _ => (),
+        }
+    }
+
+    Ok(())
 }
